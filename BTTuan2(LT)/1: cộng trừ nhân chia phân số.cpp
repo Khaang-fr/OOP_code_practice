68152b60e7e2224777f419e76a6e6b5a2bfe2151
Khang <hatapuka@gmail.com>
2025-03-06 15:00:45 +0700

    Update 1: cộng trừ nhân chia phân số.cpp

--- BTTuan2(LT)/1: cộng trừ nhân chia phân số.cpp
+++ BTTuan2(LT)/1: cộng trừ nhân chia phân số.cpp
@@ -1,81 +1,69 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-struct PhanSo{
-    int tu, mau;
+class PhanSo {
+    private:
+        int tu, mau;
+    public:
+        void nhap() {
+            do {
+                cin >> tu >> mau;
+            } while (mau == 0);
+        }
+        void cong(PhanSo a, PhanSo b) {
+            tu = a.tu * b.mau + b.tu * a.mau;
+            mau = a.mau * b.mau;
+        }
+        void tru(PhanSo a, PhanSo b) {
+            tu = a.tu * b.mau - b.tu * a.mau;
+            mau = a.mau * b.mau;
+        }
+        void nhan(PhanSo a, PhanSo b) {
+            tu = a.tu * b.tu;
+            mau = a.mau * b.mau;
+        }
+        void chia(PhanSo a, PhanSo b) {
+            tu = a.tu * b.mau;
+            mau = a.mau * b.tu;
+        }
+        void rutgon() {
+            int a = tu, b = mau;
+            while (b != 0) {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+            tu /= a;
+            mau /= a;
+            if (mau < 0) {
+                tu = -tu;
+                mau = -mau;
+            }
+            if (mau == 1) cout << tu;
+            else cout << tu << "/" << mau;
+        }
+        void xuat() {
+            if (mau == 1) cout << tu;
+            else cout << tu << "/" << mau;
+        }
 };
 
-PhanSo operator+(PhanSo a, PhanSo b) {
+int main() {
+    PhanSo a, b;
+    a.nhap();
+    b.nhap();
     PhanSo c;
-    c.tu = a.tu * b.mau + b.tu * a.mau;
-    c.mau = a.mau * b.mau;
-    return c;
-}
-
-PhanSo operator-(PhanSo a, PhanSo b) {
-    PhanSo c;
-    c.tu = a.tu * b.mau - b.tu * a.mau;
-    c.mau = a.mau * b.mau;
-    return c;
-}
-
-PhanSo operator*(PhanSo a, PhanSo b) {
-    PhanSo c;
-    c.tu = a.tu * b.tu;
-    c.mau = a.mau * b.mau;
-    return c;
-}
-
-PhanSo operator/(PhanSo a, PhanSo b) {
-    PhanSo c;
-    c.tu = a.tu * b.mau;
-    c.mau = a.mau * b.tu;
-    return c;
-}
-
-int GCD(int a, int b) {
-    while (b != 0) {
-        int r = a % b;
-        a = b;
-        b = r;
-    }
-    return a;
-}
-
-PhanSo Rutgon(PhanSo a) {
-    int gcd = GCD(a.tu, a.mau);
-    a.tu /= gcd;
-    a.mau /= gcd;
-    if (a.mau < 0) {
-        a.tu = -a.tu;
-        a.mau = -a.mau;
-    }
-    return a;
-}
-
-int main () {
-    PhanSo a, b;   
-    do {
-       cin >> a.tu >> a.mau >> b.tu >> b.mau;
-    } while (a.mau == 0 || b.mau==0);
-    PhanSo c = a +b;
-    PhanSo d = a- b;
-    PhanSo e = a * b;
-    PhanSo f = a / b;
-    c = Rutgon(c);
-    d = Rutgon(d);
-    e = Rutgon(e);
-    f = Rutgon(f);
-    if (c.mau == 1) cout << c.tu;
-    else cout << c.tu << "/" << c.mau;
-    cout << endl;
-    if (d.mau == 1) cout << d.tu;
-    else cout << d.tu << "/" << d.mau;
-    cout << endl;
-    if (e.mau == 1) cout << e.tu;
-    else cout << e.tu << "/" << e.mau;
-    cout << endl;
-    if (f.mau == 1) cout << f.tu;
-    else cout << f.tu << "/" << f.mau;
+    c.cong(a, b);
+    c.rutgon();
+    c.xuat();
+    c.tru(a, b);
+    c.rutgon();
+    c.xuat();
+    c.nhan(a, b);
+    c.rutgon();
+    c.xuat();
+    c.chia(a, b);
+    c.rutgon();
+    c.xuat();
     return 0;
 }
