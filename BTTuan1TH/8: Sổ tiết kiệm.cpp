c96308c9af9295adfbe9a05d82e0331ab707860e
Khang <hatapuka@gmail.com>
2025-03-15 15:26:13 +0700

    Update 8: Sổ tiết kiệm.cpp

--- BTTuan1TH/8: Sổ tiết kiệm.cpp
+++ BTTuan1TH/8: Sổ tiết kiệm.cpp
@@ -74,17 +74,34 @@ bool noLongerThan6Months(NgayThang ngayMoSo, NgayThang ngayHienTai) {
 }
 
 int DaysCounter(NgayThang ngayMoSo, NgayThang ngayHienTai) {
-    int count = 0;
-    if (ngayHienTai.nam == ngayMoSo.nam) {
-        if (ngayHienTai.thang == ngayMoSo.thang) {
-            count += ngayHienTai.ngay - ngayMoSo.ngay;
-        } else {
-            count += 30 - ngayMoSo.ngay + ngayHienTai.ngay;
+    int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    auto isLeapYear = [](int year) -> bool {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    };
+    
+    auto daysSince0 = [daysInMonth, isLeapYear](NgayThang date) -> int {
+        int totalDays = date.ngay;
+        
+        for (int m = 1; m < date.thang; m++) {
+            if (m == 2 && isLeapYear(date.nam)) {
+                totalDays += 29;
+            } else {
+                totalDays += daysInMonth[m];
+            }
         }
-    } else {
-        count += 30 - ngayMoSo.ngay + ngayHienTai.ngay;
-    }
-    return count;
+        
+        for (int y = 0; y < date.nam; y++) {
+            totalDays += isLeapYear(y) ? 366 : 365;
+        }
+        
+        return totalDays;
+    };
+    
+    int days1 = daysSince0(ngayMoSo);
+    int days2 = daysSince0(ngayHienTai);
+    
+    return days2 - days1;
 }
 
 class SoTietKiem {
@@ -104,6 +121,8 @@ class SoTietKiem {
         void TinhToanTienHienTai(float laiSuatNganHan, float laiSuatDaiHan);
         void SapXep(SoTietKiem stk[], int n);
         void TimKiem(SoTietKiem stk[], int n, string maSo, int CMND);
+        void RutTien(SoTietKiem stk[], int n, string maSo, double soTienRut, float laiSuatNganHan, float laiSuatDaiHan);
+        void LietKe(SoTietKiem stk[], int n);
 };
 
 void SoTietKiem::nhap() {
@@ -114,6 +133,16 @@ void SoTietKiem::nhap() {
             break;
         }
         cout << "Ma so khong hop le. Vui long nhap lai.\n";
+    }
+
+    while (true) {
+        cout << "Nhap loai tiet kiem (Ngan han/Dai han): ";
+        getline(cin, loaiTietKiem);
+        
+        if (loaiTietKiem == "Ngan han" || loaiTietKiem == "Dai han") {
+            break;
+        }
+        cout << "Loai tiet kiem khong hop le. Vui long nhap 'Ngan han' hoac 'Dai han'.\n";
     }
     
     while (true) {
@@ -186,7 +215,8 @@ void SoTietKiem::xuat() {
     cout << "Ho ten: " << hoTen << " - ";
     cout << "CMND: " << CMND << " - ";
     cout << "Ngay mo so: " << ngayMoSo.ngay << "/" << ngayMoSo.thang << "/" << ngayMoSo.nam << " - ";
-    cout << "So tien gui: " << soTienGui << endl;
+    cout << "So tien gui: " << soTienGui << " - ";
+    cout << "Tien hien tai: " << tienHienTai << endl;
 }
 
 void SoTietKiem::CapNhatLaiSuat() {
@@ -199,29 +229,172 @@ void SoTietKiem::CapNhatLaiSuat() {
 
 void SoTietKiem::TinhToanTienHienTai(float laiSuatNganHan, float laiSuatDaiHan) {
     if (loaiTietKiem == "Ngan han") {
-        tienHienTai = soTienGui * (1 + laiSuatNganHan) * DaysCounter(ngayMoSo, ngayHienTai) / 365;
+        tienHienTai = soTienGui + soTienGui * (1 + laiSuatNganHan/100) * DaysCounter(ngayMoSo, ngayHienTai) / 365;
         cout << "Tien hien tai cua so tiet kiem " << maSo << " la: " << tienHienTai << endl;
     } else {
-        tienHienTai = soTienGui * (1 + laiSuatDaiHan) * DaysCounter(ngayMoSo, ngayHienTai) / 365;
+        tienHienTai = soTienGui + soTienGui * (1 + laiSuatDaiHan/100) * DaysCounter(ngayMoSo, ngayHienTai) / 365;
         cout << "Tien hien tai cua so tiet kiem " << maSo << " la: " << tienHienTai << endl;
     }
 }
 
+void SoTietKiem::RutTien(SoTietKiem stk[], int n, string maSo, double soTienRut, float laiSuatNganHan, float laiSuatDaiHan) {
+    bool found = false;
+    
+    for (int i = 0; i < n; i++) {
+        if (stk[i].maSo == maSo) {
+            found = true;
+            
+            float laiSuatApDung = (stk[i].loaiTietKiem == "Ngan han") ? laiSuatNganHan : laiSuatDaiHan;
+            bool isEarlyWithdrawal = false;
+            
+            if (!noLongerThan6Months(stk[i].ngayMoSo, stk[i].ngayHienTai)) {
+                if (stk[i].loaiTietKiem == "Dai han") {
+                    isEarlyWithdrawal = true;
+                    cout << "CANH BAO: Rut tien truoc thoi han (chua du 6 thang)." << endl;
+                    cout << "Tien lai se duoc tinh theo lai suat ngan han." << endl;
+                    
+                    laiSuatApDung = laiSuatNganHan * 0.7; 
+                }
+            }
+            
+            stk[i].tienHienTai = stk[i].soTienGui * (1 + laiSuatApDung) * DaysCounter(stk[i].ngayMoSo, stk[i].ngayHienTai) / 365;
+            
+            if (isEarlyWithdrawal) {
+                cout << "Lai suat ap dung: " << laiSuatApDung << " (Giam do rut som)" << endl;
+                cout << "So du hien tai sau dieu chinh: " << stk[i].tienHienTai << endl;
+            }
+            
+            if (soTienRut <= 0) {
+                cout << "So tien rut khong hop le." << endl;
+                return;
+            }
+            
+            if (soTienRut > stk[i].tienHienTai) {
+                cout << "So tien rut vuot qua so du hien tai (" << stk[i].tienHienTai << ")." << endl;
+                return;
+            }
+            
+            stk[i].tienHienTai -= soTienRut;
+            cout << "Rut tien thanh cong. So du moi: " << stk[i].tienHienTai << endl;
+            return;
+        }
+    }
+    
+    if (!found) {
+        cout << "Khong tim thay so tiet kiem voi ma so " << maSo << "." << endl;
+    }
+}
 
+void SoTietKiem::SapXep(SoTietKiem stk[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            bool shouldSwap = false;
+            
+            if (stk[i].soTienGui < stk[j].soTienGui) {
+                shouldSwap = true;
+            }
+            else if (stk[i].soTienGui == stk[j].soTienGui) {
+                if (stk[i].ngayMoSo.nam > stk[j].ngayMoSo.nam) {
+                    shouldSwap = true;
+                }
+                else if (stk[i].ngayMoSo.nam == stk[j].ngayMoSo.nam && 
+                         stk[i].ngayMoSo.thang > stk[j].ngayMoSo.thang) {
+                    shouldSwap = true;
+                }
+                else if (stk[i].ngayMoSo.nam == stk[j].ngayMoSo.nam && 
+                         stk[i].ngayMoSo.thang == stk[j].ngayMoSo.thang && 
+                         stk[i].ngayMoSo.ngay > stk[j].ngayMoSo.ngay) {
+                    shouldSwap = true;
+                }
+            }
+            
+            if (shouldSwap) {
+                SoTietKiem temp = stk[i];
+                stk[i] = stk[j];
+                stk[j] = temp;
+            }
+        }
+    }
+}
+
+void SoTietKiem::TimKiem(SoTietKiem stk[], int n, string maSo, int CMND) {
+    bool found = false;
+    
+    for (int i = 0; i < n; i++) {
+        if (stk[i].maSo == maSo && stk[i].CMND == CMND) {
+            found = true;
+            cout << "Tim thay so tiet kiem: " << endl;
+            stk[i].xuat();
+            return;
+        }
+    }
+    
+    if (!found) {
+        cout << "Khong tim thay so tiet kiem phu hop." << endl;
+    }
+}
+
+void SoTietKiem::LietKe(SoTietKiem stk[], int n) {
+    NgayThang tuNgay, denNgay;
+    bool hasResults = false;
+    
+    cout << "Nhap tu ngay (dd mm yyyy): ";
+    cin >> tuNgay.ngay >> tuNgay.thang >> tuNgay.nam;
+    
+    if (!isRealDay(tuNgay.ngay, tuNgay.thang, tuNgay.nam)) {
+        cout << "Ngay khong hop le!" << endl;
+        return;
+    }
+    
+    cout << "Nhap den ngay (dd mm yyyy): ";
+    cin >> denNgay.ngay >> denNgay.thang >> denNgay.nam;
+    
+    if (!isRealDay(denNgay.ngay, denNgay.thang, denNgay.nam)) {
+        cout << "Ngay khong hop le!" << endl;
+        return;
+    }
+    
+    auto isBeforeOrEqual = [](NgayThang date1, NgayThang date2) -> bool {
+        if (date1.nam < date2.nam) return true;
+        if (date1.nam > date2.nam) return false;
+        if (date1.thang < date2.thang) return true;
+        if (date1.thang > date2.thang) return false;
+        return date1.ngay <= date2.ngay;
+    };
+    
+    cout << "\nDanh sach so tiet kiem mo tu ngay " 
+         << tuNgay.ngay << "/" << tuNgay.thang << "/" << tuNgay.nam
+         << " den ngay " 
+         << denNgay.ngay << "/" << denNgay.thang << "/" << denNgay.nam << ":\n";
+    
+    for (int i = 0; i < n; i++) {
+        if (isBeforeOrEqual(tuNgay, stk[i].ngayMoSo) && isBeforeOrEqual(stk[i].ngayMoSo, denNgay)) {
+            stk[i].xuat();
+            hasResults = true;
+        }
+    }
+    
+    if (!hasResults) {
+        cout << "Khong tim thay so tiet kiem nao trong khoang thoi gian nay.\n";
+    }
+}
 
 int main() {
-    int n; cin >> n;
+    freopen("input.txt" , "r", stdin);
+
+    int n;
+    cout << "Nhap so luong so tiet kiem: ";
+    cin >> n;
     cin.ignore();
-    SoTietKiem *stk = new SoTietKiem[n];
+    
+   SoTietKiem *stk = new SoTietKiem[n];    
     for (int i = 0; i < n; i++) {
+        cout << "\nNhap thong tin so tiet kiem thu " << i + 1 << ":" << endl;
         stk[i].nhap();
     }
-    for (int i = 0; i < n; i++) {
-        stk[i].xuat();
-    }
-    float laiSuatNganHan;
-    float laiSuatDaiHan;
-    cout << "Nhap lai suat ngan han: ";
+        
+    float laiSuatNganHan, laiSuatDaiHan;
+    cout << "\nNhap lai suat ngan han: ";
     cin >> laiSuatNganHan;
     cout << "Nhap lai suat dai han: ";
     cin >> laiSuatDaiHan;
@@ -229,32 +402,65 @@ int main() {
         stk[i].CapNhatLaiSuat();
     }
     
-    cout << "Vui long chon hanh dong ban muon thuc hien: \n";
-    cout << "1. Tinh toan tien hien tai\n";
-    cout << "2. Sap xep\n";
-    cout << "3. Tim kiem\n";
-    int x; cin >> x;
-    switch (x) {
-        case 1:
-            for (int i = 0; i < n; i++) {
-                stk[i].TinhToanTienHienTai(laiSuatNganHan, laiSuatDaiHan);
-            }
-            break;
-        case 2:
-            stk->SapXep(stk, n);
-            for (int i = 0; i < n; i++) {
-                stk[i].xuat();
-            }
-            break;
-        case 3:
-            string maSo;
-            int CMND;
-            cout << "Nhap ma so: ";
-            cin >> maSo;
-            cout << "Nhap CMND: ";
-            cin >> CMND;
-            stk->TimKiem(stk, n, maSo, CMND);
-            break;
+    for (int i = 0; i < n; i++) {
+        stk[i].TinhToanTienHienTai(laiSuatNganHan, laiSuatDaiHan);
     }
+
+    stk->SapXep(stk, n);
+    
+    cout << "\nDanh sach so tiet kiem sau khi sap xep theo so tien gui:" << endl;
+    for (int i = 0; i < n; i++) {
+        stk[i].xuat();
+    }
+    
+    int choice = 0;
+    do {
+        cout << "\nVui long chon hanh dong ban muon thuc hien: \n";
+        cout << "1. Tim kiem\n";
+        cout << "2. Rut tien\n";
+        cout << "3. Liet ke so tiet kiem\n";
+        cout << "0. Thoat\n";
+        cout << "Chon: ";
+        cin >> choice;
+        cin.ignore();
+        
+        switch (choice) {
+            case 1: {
+                string maSoTim;
+                int CMNDTim;
+                cout << "Nhap ma so: ";
+                cin >> maSoTim;
+                cout << "Nhap CMND: ";
+                cin >> CMNDTim;
+                stk->TimKiem(stk, n, maSoTim, CMNDTim);
+                break;
+            }
+            
+            case 2: {
+                string maSoRut;
+                double soTienRut;
+                cout << "Nhap ma so: ";
+                cin >> maSoRut;
+                cout << "Nhap so tien rut: ";
+                cin >> soTienRut;
+                stk->RutTien(stk, n, maSoRut, soTienRut, laiSuatNganHan, laiSuatDaiHan);
+                break;
+            }
+
+            case 3:
+                stk->LietKe(stk, n);
+                break;
+            
+            case 0:
+                cout << "Tam biet!" << endl;
+                break;
+                
+            default:
+                cout << "Lua chon khong hop le. Vui long chon lai." << endl;
+        }
+    } while (choice != 0);
+    
+    delete[] stk;
+    fclose(stdin);
     return 0;
 }
