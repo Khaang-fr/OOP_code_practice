a3bf0acc078182e49cbf318d91a293bee2b0c0f7
Khang <hatapuka@gmail.com>
2025-03-18 16:50:34 +0700

    Update 6: mảng 2 số nguyên có 1 mảng con.cpp

--- BTTuan1TH/6: mảng 2 số nguyên có 1 mảng con.cpp
+++ BTTuan1TH/6: mảng 2 số nguyên có 1 mảng con.cpp
@@ -3,32 +3,46 @@
 using namespace std;
 
 int main() {
-    int na, nb; cin>> na >> nb;
+    int na, nb; 
+    cin >> na >> nb;
+    
+    if (na == 0 || nb == 0 || na > nb) { //Loại trường hợp mảng rỗng
+        cout << "0" << endl;
+        return 0;
+    }
+
     vector<int> a(na);
     for (auto &num : a) {
         cin >> num;
     }
+
     vector<int> b(nb);
     for (auto &num : b) {
         cin >> num;
     }
+
     vector<int> c;
-    int count =0 ;
-    for (int i = 0; i < nb; i++) {
-        if (a[0] == b[i]) {
+    int count = 0;
+
+    for (int i = 0; i <= nb - na; i++) {
+        if (a[0] == b[i]) {    //Tìm phần tử đầu của mảng a trên mảng b
+            bool match = true; 
             for (int j = 0; j < na; j++) {
-                if (a[j] != b[j+i]) {
+                if (a[j] != b[j + i]) {  //Kiểm tra các phần tử tiếp theo có giống mảng a không 
+                    match = false;
                     break;
-                } if (j== na-1) {
-                    count++;
-                    c.push_back(i);
                 }
+            }
+            if (match) {
+                count++; 
+                c.push_back(i);
             }
         }
     }
+
     cout << count << endl;
     for (int i = 0; i < count; i++) {
-        cout << c[i] << " ";
+        cout << c[i] << (i == count - 1 ? "\n" : " ");
     }
     return 0;
 }
