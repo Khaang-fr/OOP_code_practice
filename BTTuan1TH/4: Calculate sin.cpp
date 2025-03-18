dc7ba2c0017adbf2af8f04980438c84e3c348cab
Khang <hatapuka@gmail.com>
2025-03-18 10:23:40 +0700

    Update 4: Calculate sin.cpp

--- BTTuan1TH/4: Calculate sin.cpp
+++ BTTuan1TH/4: Calculate sin.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 double Sin(double x, double accuracy) {
+    // Chuẩn hóa x vào khoảng [-2π, 2π] để tăng tính chính xác
+    double PI = 3.14159265358979323846;
+    x = fmod(x, 2 * PI);
+    
     double term = x;
     double res = x;
     int n = 1;
-
+    
     while (fabs(term) >= accuracy) {
         term *= -x * x / ((2 * n) * (2 * n + 1));
         res += term;
         n++;
+        // Đảm bảo không bị lặp vô hạn
+        if (n > 100) break;
     }
-
     return res;
 }
 
@@ -20,8 +26,6 @@ int main() {
     double x;
     cin >> x;
     double result = Sin(x, 0.00001);
-
-    cout << result << endl;
-
+    cout << fixed << setprecision(5) << result << endl;
     return 0;
 }
