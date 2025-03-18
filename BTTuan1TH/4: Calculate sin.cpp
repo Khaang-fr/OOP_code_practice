7f2311447a43c2d0436b4911c09257e0c1b16034
Khang <hatapuka@gmail.com>
2025-03-18 14:34:20 +0700

    Update 4: Calculate sin.cpp

--- BTTuan1TH/4: Calculate sin.cpp
+++ BTTuan1TH/4: Calculate sin.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 double Sin(double x, double accuracy) {
@@ -24,6 +25,6 @@ int main() {
     double x;
     cin >> x;
     double result = Sin(x, 0.00001);
-    cout << result << endl;
+    cout << fixed << setprecision(5) << result << endl;
     return 0;
 }
