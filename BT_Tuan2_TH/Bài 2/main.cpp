fab488ddc5d3bed40ba108a6f6e5b8b3a66f1c30
Khang <hatapuka@gmail.com>
2025-04-01 21:19:16 +0700

    Update main.cpp

--- BT_Tuan2_TH/Bài 2/main.cpp
+++ BT_Tuan2_TH/Bài 2/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include "PhanSo.h"
+using namespace std;
 
+int main() {
+    PhanSo ps;
+    ps.Nhap();
+    cout << "Phan so vua nhap: ";
+    ps.Xuat();
+    cout << "Rut gon phan so: ";
+    ps.RutGon();
+    ps.Xuat();
+    cout << "Tinh tong voi phan so khac:" << endl;
+    ps.Tong();
+    ps.Xuat();
+    cout << "Tinh hieu voi phan so khac:" << endl;
+    ps.Hieu();
+    ps.Xuat();
+    cout << "Tinh tich voi phan so khac:" << endl;
+    ps.Tich();
+    ps.Xuat();
+    cout << "Tinh thuong voi phan so khac:" << endl;
+    ps.Thuong();
+    ps.Xuat();
+    cout << "So sanh voi phan so khac:" << endl;
+    ps.SoSanh();
+    return 0;
+}
