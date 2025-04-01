b2a429b113ead8c283d0ee0ab048b8b696cd4055
Khang <hatapuka@gmail.com>
2025-04-01 21:18:27 +0700

    Create PhanSo.h

---
+++ BT_Tuan2_TH/Bài 2/PhanSo.h
@@ -0,0 +1,15 @@
+#pragma once
+class PhanSo
+{
+private:
+	int iTu, iMau;
+public:
+	void Nhap();
+	void Xuat() const;
+	void RutGon();
+	void Tong();
+	void Hieu();
+	void Tich();
+	void Thuong();
+	void SoSanh();
+};
