2b23362cffa8d594bb8e5e8ffebc953551c520b6
Khang <hatapuka@gmail.com>
2025-04-14 21:13:41 +0700

    Create Holder.cpp

---
+++ BT_Tuan3_TH/Bài 3/Holder.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+class cDSPhanSo {
+    private:
+        int n;
+        int *tu;
+        int *mau;
+    public:
+        cDSPhanSo();
+        cDSPhanSo(int n);
+        ~cDSPhanSo();
+        void Nhap();
+        void Xuat();
+        void RutGon();
+        void Tong();
+        void Max();
+        void Min();
+        void TuLaSoNguyenToLonNhat();
+        void SapXepTangDan();
+        void SapXepGiamDan();
+};
+
+cDSPhanSo::cDSPhanSo() {
+
+}
+cDSPhanSo::cDSPhanSo(int n) {
+
+}
+cDSPhanSo::~cDSPhanSo() {
+
+}
+void cDSPhanSo::Nhap() {
+
+}
+void cDSPhanSo::Xuat() {
+
+}
+void cDSPhanSo::RutGon() {
+
+}
+void cDSPhanSo::Tong() {
+
+}
+void cDSPhanSo::Max() {
+
+}
+void cDSPhanSo::Min() {
+
+}
+void cDSPhanSo::TuLaSoNguyenToLonNhat() {
+
+}
+void cDSPhanSo::SapXepTangDan() {
+
+}
+void cDSPhanSo::SapXepGiamDan() {
+
+}
