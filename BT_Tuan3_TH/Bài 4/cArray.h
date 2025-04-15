e0dd05079da2a80776137aba856c62a075c36e82
Khang <hatapuka@gmail.com>
2025-04-15 16:16:45 +0700

    Create cArray.h

---
+++ BT_Tuan3_TH/Bài 4/cArray.h
@@ -0,0 +1,15 @@
+#pragma once
+class cArray {
+    private:
+        int n;
+        int *a;
+    public:
+        void Nhap();
+        void Xuat();
+        void CountX();
+        bool isAscending();
+        void smallestOdd();
+        void largestPrime();
+        void SapXepTangDan();
+        void SapXepGiamDan();
+};
