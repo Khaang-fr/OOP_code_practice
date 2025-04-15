dfb2a3be3a61ac276a6a52f198eb307b5325a449
Khang <hatapuka@gmail.com>
2025-04-15 15:31:19 +0700

    Create main.cpp

---
+++ BT_Tuan3_TH/Bài 3/main.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include "cDSPhanSo.h"
+using namespace std;
+
+int main() {
+    cDSPhanSo ds;
+    ds.Nhap();
+    ds.Xuat();
+    ds.Tong();
+    ds.Max();
+    ds.Min();
+    ds.TuLaSoNguyenToLonNhat();
+    ds.SapXepTangDan();
+    ds.SapXepGiamDan();
+    return 0;
+}
