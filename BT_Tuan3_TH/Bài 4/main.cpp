d5f58d26dba3cffef2820fe27e1c27b6b278708e
Khang <hatapuka@gmail.com>
2025-04-15 16:17:46 +0700

    Create main.cpp

---
+++ BT_Tuan3_TH/Bài 4/main.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cArray arr;
+    arr.Nhap();
+    cout << "Mang da nhap: ";
+    arr.Xuat();
+    arr.CountX();
+    if (arr.isAscending()) {
+        cout << "Mang da sap xep tang dan." << endl;
+    } else {
+        cout << "Mang khong sap xep tang dan." << endl;
+    }
+    arr.smallestOdd();
+    arr.largestPrime();
+    arr.SapXepTangDan();
+    arr.SapXepGiamDan();
+    return 0;
+}
