d38a364facd5f9b0f7204c8f2a879f7b225378e3
Khang <hatapuka@gmail.com>
2025-05-08 08:58:04 +0700

    Create main.cpp

---
+++ BT_Tuan4_TH/Bài 5/main.cpp
@@ -0,0 +1,11 @@
+##include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include "QuanLy.h"
+using namespace std;
+int main() {
+    QuanLy quanLy;
+    quanLy.menu();
+    return 0;
+}
