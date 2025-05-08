602cb69fde45cdbb1414da2c8c2082675a70fd1b
Khang <hatapuka@gmail.com>
2025-05-08 09:03:39 +0700

    Create SoPhuc.cpp

---
+++ BT_Tuan5_TH/Bài 1/SoPhuc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include "SoPhuc.h"
+SoPhuc SoPhuc::operator+(const SoPhuc& b) const {
+    SoPhuc result;
+    result.thuc = this->thuc + b.thuc;
+    result.ao = this->ao + b.ao;
+    return result;
+}
+SoPhuc SoPhuc::operator-(SoPhuc b) {
+    SoPhuc result;
+    result.thuc = this->thuc - b.thuc;
+    result.ao = this->ao - b.ao;
+    return result;
+}
+SoPhuc SoPhuc::operator*(SoPhuc b) {
+    SoPhuc result;
+    result.thuc = this->thuc * b.thuc - this->ao * b.ao;
+    result.ao = this->thuc * b.ao + this->ao * b.thuc;
+    return result;
+}
+SoPhuc SoPhuc::operator/(SoPhuc b) {
+    SoPhuc result;
+    float denominator = b.thuc * b.thuc + b.ao * b.ao;
+    result.thuc = (this->thuc * b.thuc + this->ao * b.ao) / denominator;
+    result.ao = (this->ao * b.thuc - this->thuc * b.ao) / denominator;
+    return result;
+}
