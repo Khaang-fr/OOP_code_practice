7111714f39a5d793348272f51fe8e08fd9bcd0d9
Khang <hatapuka@gmail.com>
2025-05-08 09:26:03 +0700

    Update main.cpp

--- BT_Tuan5_TH/Bài 1/main.cpp
+++ BT_Tuan5_TH/Bài 1/main.cpp
@@ -13,6 +13,9 @@ int main()
     cout << "Tich: " << c << endl;
     c = a / b;
     cout << "Thuong: " << c << endl;
-    
+    bool equal = a == b;
+    cout << "So phuc a " << (equal ? "bang" : "khong bang") << " so phuc b" << endl;
+    bool notEqual = a != b;
+    cout << "So phuc a " << (notEqual ? "khong bang" : "bang") << " so phuc b" << endl;
     return 0;
 }
